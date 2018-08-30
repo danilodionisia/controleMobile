cbef2a92d2bde48807b8a9bd77f996db493e34eb
Danilo Dionisia <danilo.dionisia@yahoo.com.br>
2018-08-30 10:59:06 -0300

    Add files via upload

---
+++ Flash.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Importe essa biblioteca
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Flash : MonoBehaviour {
+
+    Rigidbody2D rb;
+    public float velX, velY;
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+	}
+	
+	
+	void Update () {
+        //pega o movimento na horizontal
+        velX = CrossPlatformInputManager.GetAxis("Horizontal");
+        //pega o movimento na vertival
+        velY = CrossPlatformInputManager.GetAxis("Vertical");
+
+        if (velX < 0)
+        {
+            transform.localScale = new Vector2(-0.210174f, 0.3999445f);
+        }
+        else
+        {
+            transform.localScale = new Vector2(0.210174f, 0.3999445f);
+        }
+
+        //movimenta o player de acordo com os toques nos botões
+        rb.velocity = new Vector2(velX * 50f, velY * 50f);
+	}
+}
